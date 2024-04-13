b34617b7af3127b265d28e749463a664b129807c
Joshua B <desertflame3@gmail.com>
2024-04-13 18:29:00 -0400

    Update PWA2024.js

--- PWA2024.js
+++ PWA2024.js
@@ -1,3 +1,18 @@
+Skip to content
+ 
+Search…
+All gists
+Back to GitHub
+@therysin
+therysin/PWA2024.js
+Last active now
+Code
+Revisions
+2
+Clone this repository at &lt;script src=&quot;https://gist.github.com/therysin/41a95294e55b67f1536a53d6ef6a633a.js&quot;&gt;&lt;/script&gt;
+<script src="https://gist.github.com/therysin/41a95294e55b67f1536a53d6ef6a633a.js"></script>
+PWA2024 Userscript
+PWA2024.js
 // ==UserScript==
 // @name      PWA2024
 // @author    Therysin
@@ -19,6 +34,7 @@ const webManifest = {
 
 let existingMetaTag1 = document.querySelector('meta[name="apple-mobile-web-app-capable"]');
 let existingMetaTag2 = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
+let asuramenu = document.querySelector('.mm');
 
 const manifestElem = document.createElement('link');
 manifestElem.setAttribute('rel', 'manifest');
@@ -48,3 +64,23 @@ if (existingMetaTag2) {
     newMetaTag2.setAttribute('content', 'black-translucent');
     document.head.appendChild(newMetaTag2);
 }
+
+if (asuramenu) {
+  asuramenu.setAttribute('style', 'background-color:black')
+}
+@therysin
+Comment
+ 
+Leave a comment
+ 
+Footer
+© 2024 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
+Manage cookies
+Do not share my personal information
