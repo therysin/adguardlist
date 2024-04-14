70d3113b565b84ecda1b16ddccfa0f8f19748066
Joshua B <desertflame3@gmail.com>
2024-04-14 12:25:19 -0400

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,10 @@
+let asuramenu = document.querySelector('.nav');
+let asuramenumobile = document.querySelector('.th');
+
+if (asuramenu) {
+  asuramenu.setAttribute('style', 'background-color:black')
+}
+
+if (asuramenumobile) {
+  asuramenumobile.setAttribute('style', 'background-color:black')
+}
